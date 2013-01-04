8ada16b39ac1d492cee09f65ce08b2e16b146d7a
Goran Mekić <goran.mekic@saturized.com>
2013-01-04 15:04:24 +0100

    Fix typo (only breaks 32bit systems?).

--- config.fish
+++ config.fish
@@ -1,11 +1,11 @@
 set -x LC_COLLATE "POSIX"
 set -x CDPATH "."
 
-if [ $TERM == 'rxvt-unicode-256color' ]
+if [ $TERM = 'rxvt-unicode-256color' ]
     set -x TERM rxvt
 end
 
-if [ $TERM == 'rxvt' ]
+if [ $TERM = 'rxvt' ]
     set dircolors_file ~/.config/fish/DIR_COLORS.256
     if [ -e $dircolors_file ]
         eval (dircolors -c $dircolors_file)
@@ -13,3 +13,5 @@ if [ $TERM == 'rxvt' ]
 else
     set LS_COLORS ''
 end
+
+bind \ec 'kill-word'
