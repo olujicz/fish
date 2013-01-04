d5d70d653e9a5ecad487d55e3ca8096da0751fbb
Goran Mekić <goran.mekic@saturized.com>
2013-01-04 12:42:26 +0100

    Check what $TERM is, and not isn't.

--- config.fish
+++ config.fish
@@ -1,14 +1,13 @@
 set -x LC_COLLATE "POSIX"
 set -x CDPATH "."
 
-if [ $TERM != 'linux' ;and $TERM != 'xterm-256color' ]
+if [ $TERM == 'rxvt-unicode-256color' ]
+    set -x TERM rxvt
+end
+
+if [ $TERM == 'rxvt' ]
     set dircolors_file ~/.config/fish/DIR_COLORS.256
     if [ -e $dircolors_file ]
         eval (dircolors -c $dircolors_file)
     end
 end
-
-if [ $TERM == 'rxvt-unicode-256color' ]
-    set -x TERM rxvt
-end
-
