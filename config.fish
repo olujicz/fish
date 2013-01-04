d933f389331878a3d11cc78b38de3fb5d1c8630a
Goran Mekić <goran.mekic@saturized.com>
2013-01-04 12:39:21 +0100

    Fix logic.

--- config.fish
+++ config.fish
@@ -1,7 +1,7 @@
 set -x LC_COLLATE "POSIX"
 set -x CDPATH "."
 
-if [ $TERM != 'linux'; or $TERM != 'xterm-256color' ]
+if [ $TERM != 'linux' ;and $TERM != 'xterm-256color' ]
     set dircolors_file ~/.config/fish/DIR_COLORS.256
     if [ -e $dircolors_file ]
         eval (dircolors -c $dircolors_file)
